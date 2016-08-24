4c20fe652f6c8a3d8eba839853dc54abd85745ba
littlewin <littlewin.wang@gmail.com>
2016-08-24 14:07:24 +0800

    update btn & icon-btn

---
+++ src/components/btn.vue
@@ -0,0 +1,34 @@
+<template>
+  <button :class="['topcoat-button' + isLarge + isQuiet + isCta]">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    large: {
+      type: Boolean
+    },
+    quiet: {
+      type: Boolean
+    },
+    cta: {
+      type: Boolean
+    },
+    text: {
+      type: String,
+      default: 'Button'
+    }
+  },
+  computed: {
+    isLarge: function() {
+      return this.large ? '--large' : '' 
+    },
+    isQuiet: function() {
+      return (!this.cta && this.quiet) ? '--quiet' : '' 
+    },
+    isCta: function() {
+      return this.cta ? '--cta' : ''
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
 import Vue from 'vue/dist/vue.js'
-import btnbar from './components/btnbar.vue'
+
+import btnbar from './components/btn-bar.vue'
+import btn from './components/btn.vue'
+import iconbtn from './components/icon-btn.vue'
 
 Vue.component('btn-bar', btnbar)
+Vue.component('btn', btn)
+Vue.component('icon-btn', iconbtn)
 
 new Vue({
   el: '#app',
   data() {
     return { 
-      list: ['A', 'B', 'C']
+      list: ['A', 'B', 'C'],
+      large: true,
+      quiet: false,
+      cta: false,
+      disable: 'disabled'
     }
   }
 })
